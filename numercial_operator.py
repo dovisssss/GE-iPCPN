79f69183244389c3b2bdbfa581bbec4c08583f94
cx <2680670177@qq.com>
2025-04-28 15:25:05 +0800

    add duffing_datagen

---
+++ numercial_operator.py
@@ -0,0 +1,41 @@
+import torch
+from einops import repeat
+
+if torch.cuda.is_available():
+    device = torch.device("cuda")  # GPU
+else:
+    device = torch.device("cpu")   # CPU
+
+def integration_operator(batch, seq_len, dt):
+    phi_1 = torch.tril(torch.ones(seq_len, seq_len, device=device))
+    phi_1[0,0] = 0.0
+
+    phi_2 = torch.tril(torch.ones(seq_len, seq_len, device=device))
+    phi_2[:,0] = 0.0
+    phi_2.fill_diagonal_(0.0)
+
+    phi = (phi_1 + phi_2) * dt / 2
+    Phi = repeat(phi, "row col -> batch row col", batch=batch)
+
+    return Phi
+
+def differentiation_operator(batch, seq_len, dt):
+    phi_1_part1 = torch.tensor([[-3/2, 2, -1/2]], dtype=torch.float32, device=device)
+    phi_1_part2 = torch.zeros(1, seq_len - 3, device=device)
+    phi_1 = torch.cat([phi_1_part1, phi_1_part2], dim=1)
+
+    temp_1 = -0.5 * torch.eye(seq_len - 2, dtype=torch.float32, device=device)
+    temp1_pad = torch.cat([temp_1, torch.zeros(seq_len - 2, 2)], dim=1)
+    temp_2 = 0.5 * torch.eye(seq_len - 2, dtype=torch.float32, device=device)
+    temp2_pad = torch.cat([torch.zeros(seq_len - 2, 2), temp_2], dim=1)
+    phi_2 = temp1_pad + temp2_pad
+
+    phi_3_part1 = torch.zeros(1, seq_len - 3,  device=device)
+    phi_3_part2 = torch.tensor([[-1/2, 2, -3/2]], dtype=torch.float32, device=device)
+    phi_3 = torch.cat([phi_3_part1, phi_3_part2], dim=1)
+
+    phi = torch.cat([phi_1, phi_2, phi_3], dim=0) #按行拼接dim=0
+    phi = (1 / dt) * phi
+    Phi = repeat(phi, "row col -> batch row col", batch=batch)
+
+    return Phi
