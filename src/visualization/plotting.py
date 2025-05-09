b8c15de26bde9b25307fb3b3ac80951e3913eeed
cx <2680670177@qq.com>
2025-05-09 20:48:29 +0800

    modify plot color

--- src/visualization/plotting.py
+++ src/visualization/plotting.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import torch
 
 def plot_training_data(clean_data, noisy_data, title, training_figure_path):
+    plt.rcParams["font.family"] = "Times New Roman"
     clean_instance = clean_data[0].cpu().numpy() if torch.is_tensor(clean_data[0]) else clean_data[0]
     noisy_instance = noisy_data[0].cpu().numpy() if torch.is_tensor(noisy_data[0]) else noisy_data[0]
 
@@ -10,22 +11,39 @@ def plot_training_data(clean_data, noisy_data, title, training_figure_path):
     rms_ratio = rms_1 / rms_2 if rms_2 != 0 else 0
 
     plt.figure()
-    plt.plot(clean_instance, label="Clean x")
-    plt.plot(noisy_instance, label="Noisy x")
-    plt.plot(clean_instance - noisy_instance, label="Error x")
+    plt.plot(clean_instance, color="#205EA7",label="Clean x")
+    plt.plot(noisy_instance, linestyle="--", color="#F8A0C9",label="Noisy x")
+    plt.plot(clean_instance - noisy_instance,color="#650233", label="Error x")
+    plt.grid(
+        True,
+        linestyle=":",
+        color="gray",
+        alpha=0.6,
+        linewidth=0.5
+    )
     plt.title(f"Training {title}, {rms_ratio:.4f}")
     plt.legend()
     plt.savefig(training_figure_path)
 
 def plot_loss_curve(loss_training_log, loss_validation_log, title,  training_figure_path):
+    plt.rcParams["font.family"] = "Times New Roman"
     plt.figure()
-    plt.semilogy(loss_training_log, label="Training Loss")
-    plt.semilogy(loss_validation_log, label="Validation Loss")
+    plt.semilogy(loss_training_log, color="#205EA7",label="Training Loss")
+    plt.semilogy(loss_validation_log, color="#650233",label="Validation Loss")
+    plt.grid(
+        True,
+        linestyle=":",
+        color="gray",
+        alpha=0.6,
+        linewidth=0.5
+    )
     plt.title(f"Loss curve of {title}")
     plt.legend()
     plt.savefig(training_figure_path)
 
 def plot_prediction(prediction, clean_data, title, prediction_figure_path):
+    plt.rcParams["font.family"] = "Times New Roman"
+
     if torch.is_tensor(prediction):
         prediction = prediction.detach().cpu().numpy()
     if torch.is_tensor(clean_data):
@@ -39,9 +57,16 @@ def plot_prediction(prediction, clean_data, title, prediction_figure_path):
     rms_ratio = rms_1 / rms_2 if rms_2 != 0 else 0
 
     plt.figure()
-    plt.plot(prediction_instance, label="Predicted")
-    plt.plot(clean_instance, label="True")
-    plt.plot(prediction_instance - clean_instance, label="Error")
+    plt.plot(clean_instance, color="#205EA7", label="True")
+    plt.plot(prediction_instance, linestyle="--", color="#F8A0C9", label="Predicted")
+    plt.plot(prediction_instance - clean_instance, color="#650233",label="Error")
+    plt.grid(
+        True,
+        linestyle=":",
+        color="gray",
+        alpha=0.6,
+        linewidth=0.5
+    )
     plt.title(f" {title} Prediction, RMS {rms_ratio:.4f}")
     plt.legend()
     plt.savefig(prediction_figure_path)
